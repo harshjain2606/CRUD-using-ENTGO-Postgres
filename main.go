72e0db230e76355e70f41abccaf3796c929ac816
harshjain2606 <jaainharsh383@gmail.com>
2025-08-01 23:18:48 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"ent_postgres_crud/ent"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	client, err := ent.Open("postgres", "host=localhost port=5432 user=postgres dbname=entdb password=Haarsh@123 sslmode=disable")
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	defer client.Close()
+
+	ctx := context.Background()
+
+	// Auto migrate
+	if err := client.Schema.Create(ctx); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	// CREATE
+	user, err := client.User.Create().
+		SetName("Harsh").
+		SetEmail("hhh@example.com").
+		Save(ctx)
+	if err != nil {
+		log.Fatalf("failed creating user: %v", err)
+	}
+	fmt.Println("User created:", user)
+
+	// READ
+	readUser, err := client.User.Get(ctx, user.ID)
+	if err != nil {
+		log.Fatalf("failed reading user: %v", err)
+	}
+	fmt.Println("User read:", readUser)
+
+	// UPDATE
+	updatedUser, err := client.User.
+		UpdateOneID(user.ID).
+		SetName("Harsh Updated").
+		Save(ctx)
+	if err != nil {
+		log.Fatalf("failed updating user: %v", err)
+	}
+	fmt.Println("User updated:", updatedUser)
+
+	//DELETE
+	// err = client.User.DeleteOneID(user.ID).Exec(ctx)
+	// if err != nil {
+	// 	log.Fatalf("failed deleting user: %v", err)
+	// }
+	// fmt.Println("User deleted")
+}
